949f6f3622f8638d963f910b2149be85654fcaaf
Darren Evans <darrenrevans10@gmail.com>
2018-01-23 21:05:27 -0500

    updated server.js & .gitignore

---
+++ server.js
@@ -0,0 +1,32 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+var db = require("./models");
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+require("./routes/api-routes.js")(app);
+require("./routes/handlebar-routes.js")(app);
+
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
