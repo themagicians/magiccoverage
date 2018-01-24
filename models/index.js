94d4a6cf941dce2af850858dea8a4a54ad3810dd
Darren Evans <darrenrevans10@gmail.com>
2018-01-23 21:44:53 -0500

    created .env

--- models/index.js
+++ models/index.js
@@ -5,13 +5,15 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/..\config\config.json')[env];
+var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
+
+require('dotenv').config();
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, process.env.DB_PASS, config);
 }
 
 fs
