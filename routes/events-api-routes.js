700595f5eb73dd1ef5c0893d82a4687235e3deac
Darren Evans <darrenrevans10@gmail.com>
2018-01-26 14:31:50 -0500

    updated events routes

--- routes/events-api-routes.js
+++ routes/events-api-routes.js
@@ -1,3 +1,43 @@
-// var db = require("../models");
+var db = require("../models");
+
+module.exports = function(app) {
+	app.get("/api/events", function(req, res) {
+		db.Events.findAll({
+			include: [db.Rounds]
+		}).then(function(dbEvents) {
+			res.json(dbEvents);
+		});
+	});
+
+	app.get("/api/events/:eventName", function(req, res) {
+		db.Events.findOne({
+			where: {
+				event_name: req.params.eventName
+			},
+			include: [db.Rounds]
+		}).then(function(dbEvents) {
+			res.json(dbEvents);
+		});
+	});
+
+	app.post("/api/events", function(req, res) {
+		//I'm not positive if rounds need to be included in this event creation
+		db.Events.create(req.body).then(function(dbEvents) {
+			res.json(dbEvents);
+		});
+	});
+
+	app.delete("/api/events/:eventName", function(req, res) {
+		db.Events.destroy({
+			where: {
+				event_name: req.params.eventName
+			}
+		}).then(function(dbEvents) {
+			res.json(dbEvents);
+		});
+	});
+
+};
+
 
 
