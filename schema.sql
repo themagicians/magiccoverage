c49146728df2ff7ace568b662cfda6229651f8e0
Charlie Bowden <charles.bowden@gmail.com>
2018-01-25 20:18:55 -0500

    Proposed Final Schema

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE magic_db;
 USE magic_db;
 
 -- Create the table tasks.
-CREATE TABLE coverage
+CREATE TABLE events
 (
 	id int NOT NULL AUTO_INCREMENT,
     -- This will hold our provider either Twitch or YouTube --
@@ -18,36 +18,33 @@ CREATE TABLE coverage
     event_date DATE,
     -- Format refers to the tournament format --
     format varchar(255),
-    -- Date of the Event that is being Covered --
-	coverage_date DATE,
-    -- Stores the Colors of the Deck from search form checkboxes --
-    deck_colors varchar(255),
-    -- User Submitted Deck Title --
-    deck_name varchar(255),
-    -- URL to Match Coverage --
-    coverage_link varchar(255),
-    -- Player in Seat One --
-    player_1_name varchar(255),
-    -- Player 1 Deck Name --
-    player_1_deck varchar(255),
-    -- Player in Seat Two --
-    player_2_name varchar(255),
-    -- Player 2 Deck Name --
-    player_2_deck,
     -- Primary Key set to ID --
 	PRIMARY KEY (id)
 );
 
--- Thoughts for maybe breaking out a table for each event with a foreign key --
--- Table for players that holds the deck info?  --
-CREATE TABLE events
+
+CREATE TABLE rounds
 (
+    id int NOT NULL AUTO_INCREMENT,
+    -- Round Number --
+    round_number varchar(255),
+    -- Event Name same as events --
+    event_name varchar(255),
     -- URL to Match Coverage --
     coverage_link varchar(255),
-    -- Player in Seat One --
-    player_1 varchar(255),
-    -- Player in Seat Two --
-    player_2 varchar(255),
     -- Foreign Key --
+    Foreign KEY(event_name)
+);
 
+CREATE TABLE players
+(
+    id int NOT NULL AUTO_INCREMENT,
+    -- Round Number --
+    round_number varchar(255),
+    -- Player --
+    player_name varchar(255),
+    -- Player --
+    player_deck varchar(255),
+    -- Primary Key --
+    PRIMARY KEY(round_number)
 );
