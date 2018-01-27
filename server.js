df58c5ce376d27c594183018b4c6e80c039fa9c1
Darren Evans <darrenrevans10@gmail.com>
2018-01-27 10:34:54 -0500

    updated db

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.set("view engine", "handlebars");
 require("./routes/events-api-routes.js")(app);
 require("./routes/rounds-api-routes.js")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
