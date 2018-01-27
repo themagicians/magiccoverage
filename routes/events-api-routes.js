f878bce9c9f1e44281a6d57f309fb765f13892b0
Charlie Bowden <charles.bowden@gmail.com>
2018-01-26 20:29:32 -0500

    working html route

--- routes/events-api-routes.js
+++ routes/events-api-routes.js
@@ -1,6 +1,22 @@
+var express = require("express");
+
+var router = express.Router();
+
 var db = require("../models");
 
 module.exports = function(app) {
+	app.get("/", function (req, res) {
+		db.Events.findAll({
+			include: [db.Rounds]
+		}).then(function (dbEvents) {
+			var hbsObject = {
+				events: dbEvents
+			};
+			console.log(hbsObject);
+			res.render("index", hbsObject);
+		});
+	});
+	
 	app.get("/api/events", function(req, res) {
 		db.Events.findAll({
 			include: [db.Rounds]
