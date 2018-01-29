e37175486d9a0b404f3b3394219c139392d49f1a
Carmel Johnson <vineyardcarmel@gmail.com>
2018-01-29 17:54:01 -0500

    upload of precompiled templates and partials

--- public/assets/js/formActions.js
+++ public/assets/js/formActions.js
@@ -29,23 +29,23 @@ $(function() {
         }; //var eventData end
 
         var roundData = [
-            round1 {
-                round_number: "1",
+           {round1: {
+                round_number: 1,
                 coverage_link: $("#coverage_linkR1").val().trim(),
                 player1_name: $("#player1R1").val().trim(),
                 player2_name: $("#player2R1").val().trim(),
                 player1_deck: $("#player1deckR1").val().trim(),
                 player2_deck: $("#player2deckR1").val().trim()
-            },
-            round2 {
-                round_number: "2",
+            }},
+            {round2: {
+                round_number: 2,
                 coverage_link: $("#coverage_linkR2").val().trim(),
                 player1_name: $("#player1R2").val().trim(),
                 player2_name: $("#player2R2").val().trim(),
                 player1_deck: $("#player1deckR2").val().trim(),
                 player2_deck: $("#player2deckR2").val().trim()
-            },
-            round3 {
+            }},
+            {round3: {
                 round_number: "3",
                 coverage_link: $("#coverage_linkR3").val().trim(),
                 player1_name: $("#player1R3").val().trim(),
@@ -53,143 +53,143 @@ $(function() {
                 player1_deck: $("#player1deckR3").val().trim(),
                 player2_deck: $("#player2deckR3").val().trim()
                 
-            },
-            round4 {
+            }},
+            {round4: {
                 round_number: "4",
                 coverage_link: $("#coverage_linkR4").val().trim(),
                 player1_name: $("#player1R4").val().trim(),
                 player2_name: $("#player2R4").val().trim(),
                 player1_deck: $("#player1deckR4").val().trim(),
                 player2_deck: $("#player2deckR4").val().trim()
-            },
-            round5 {
+            }},
+            {round5: {
                 round_number: "5",
                 coverage_link: $("#coverage_linkR5").val().trim(),
                 player1_name: $("#player1R5").val().trim(),
                 player2_name: $("#player2R5").val().trim(),
                 player1_deck: $("#player1deckR5").val().trim(),
                 player2_deck: $("#player2deckR5").val().trim()
-            },
-            round6 {
+            }},
+            {round6: {
                 round_number: "6",
                 coverage_link: $("#coverage_linkR6").val().trim(),
                 player1_name: $("#player1R6").val().trim(),
                 player2_name: $("#player2R6").val().trim(),
                 player1_deck: $("#player1deckR6").val().trim(),
                 player2_deck: $("#player2deckR6").val().trim()
-            },
-            round7 {
+            }},
+            {round7: {
                 round_number: "7",
                 coverage_link: $("#coverage_linkR7").val().trim(),
                 player1_name: $("#player1R7").val().trim(),
                 player2_name: $("#player2R7").val().trim(),
                 player1_deck: $("#player1deckR7").val().trim(),
                 player2_deck: $("#player2deckR7").val().trim()
-            },
-            round8 {
+            }},
+            {round8: {
                 round_number: "8",
                 coverage_link: $("#coverage_linkR8").val().trim(),
                 player1_name: $("#player1R8").val().trim(),
                 player2_name: $("#player2R8").val().trim(),
                 player1_deck: $("#player1deckR8").val().trim(),
                 player2_deck: $("#player2deckR8").val().trim()
-            },
-            round9 {
+            }},
+            {round9: {
                 round_number: "9",
                 coverage_link: $("#coverage_linkR9").val().trim(),
                 player1_name: $("#player1R9").val().trim(),
                 player2_name: $("#player2R9").val().trim(),
                 player1_deck: $("#player1deckR9").val().trim(),
                 player2_deck: $("#player2deckR9").val().trim()
-            },
-            round10 {
+            }},
+            {round10: {
                 round_number: "10",
                 coverage_link: $("#coverage_linkR10").val().trim(),
                 player1_name: $("#player1R10").val().trim(),
                 player2_name: $("#player2R10").val().trim(),
                 player1_deck: $("#player1deckR10").val().trim(),
                 player2_deck: $("#player2deckR10").val().trim()
-            },
-            round11 {
+            }},
+            {round11: {
                 round_number: "11",
                 coverage_link: $("#coverage_linkR11").val().trim(),
                 player1_name: $("#player1R11").val().trim(),
                 player2_name: $("#player2R11").val().trim(),
                 player1_deck: $("#player1deckR11").val().trim(),
                 player2_deck: $("#player2deckR11").val().trim()
-            },
-            round12 {
+            }},
+            {round12: {
                 round_number: "12",
                 coverage_link: $("#coverage_linkR12").val().trim(),
                 player1_name: $("#player1R12").val().trim(),
                 player2_name: $("#player2R12").val().trim(),
                 player1_deck: $("#player1deckR12").val().trim(),
                 player2_deck: $("#player2deckR12").val().trim()
-            },
-            round13 {
+            }},
+            {round13: {
                 round_number: "13",
                 coverage_link: $("#coverage_linkR13").val().trim(),
                 player1_name: $("#player1R13").val().trim(),
                 player2_name: $("#player2R13").val().trim(),
                 player1_deck: $("#player1deckR13").val().trim(),
                 player2_deck: $("#player2deckR13").val().trim()
-            },
-            round14 {
+            }},
+            {round14: {
                 round_number: "14",
                 coverage_link: $("#coverage_linkR14").val().trim(),
                 player1_name: $("#player1R14").val().trim(),
                 player2_name: $("#player2R14").val().trim(),
                 player1_deck: $("#player1deckR14").val().trim(),
                 player2_deck: $("#player2deckR14").val().trim()
-            },
-            round15 {
+            }},
+            {round15: {
                 round_number: "15",
                 coverage_link: $("#coverage_linkR15").val().trim(),
                 player1_name: $("#player1R15").val().trim(),
                 player2_name: $("#player2R15").val().trim(),
                 player1_deck: $("#player1deckR15").val().trim(),
                 player2_deck: $("#player2deckR15").val().trim()
-            },
-            round16 {
+            }},
+            {round16: {
                 round_number: "16",
                 coverage_link: $("#coverage_linkR16").val().trim(),
                 player1_name: $("#player1R16").val().trim(),
                 player2_name: $("#player2R16").val().trim(),
                 player1_deck: $("#player1deckR16").val().trim(),
                 player2_deck: $("#player2deckR16").val().trim()
-            },
-            round17 {
+            }},
+            {round17: {
                 round_number: "17",
                 coverage_link: $("#coverage_linkR17").val().trim(),
                 player1_name: $("#player1R17").val().trim(),
                 player2_name: $("#player2R17").val().trim(),
                 player1_deck: $("#player1deckR17").val().trim(),
                 player2_deck: $("#player2deckR17").val().trim()
-            },
-            round18 {
+            }},
+            {round18: {
                 round_number: "18",
                 coverage_link: $("#coverage_linkR18").val().trim(),
                 player1_name: $("#player1R18").val().trim(),
                 player2_name: $("#player2R18").val().trim(),
                 player1_deck: $("#player1deckR18").val().trim(),
                 player2_deck: $("#player2deckR18").val().trim()
-            },
-            round19 {
+            }},
+            {round19: {
                 round_number: "19",
                 coverage_link: $("#coverage_linkR19").val().trim(),
                 player1_name: $("#player1R19").val().trim(),
                 player2_name: $("#player2R19").val().trim(),
                 player1_deck: $("#player1deckR19").val().trim(),
                 player2_deck: $("#player2deckR19").val().trim()
-            },
-            round20 {
+            }},
+            {round20: {
                 round_number: "20",
                 coverage_link: $("#coverage_linkR20").val().trim(),
                 player1_name: $("#player1R20").val().trim(),
                 player2_name: $("#player2R20").val().trim(),
                 player1_deck: $("#player1deckR20").val().trim(),
                 player2_deck: $("#player2deckR20").val().trim()
-            }         
+            }}         
         ]; //roundData end
     // POST functionality
         $.ajax("/events/create", {
